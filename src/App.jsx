cd9a31760a8d019c0e588c767fd48822e0cb2fad
MPSxDev <mparedes.digital@gmail.com>
2025-01-23 19:40:33 -0300

    feat: Add new page for WorldMatching game and Games directory

--- src/App.jsx
+++ src/App.jsx
@@ -11,6 +11,7 @@ import CertificationContent from './pages/CertificationContent';
 import ModuleDetails from './pages/ModuleDetails';
 import WalletConnection from './pages/WalletConnection';
 import HomePage from './pages/HomePage';
+import WordMatching from './pages/Games/WorldMatching';
 
 function App() {
   return (
@@ -27,6 +28,7 @@ function App() {
           <Route path="/starkla-chat" element={<StarklaChat />} />
           <Route path="/certification-content" element={<CertificationContent />} />
           <Route path="/module-details" element={<ModuleDetails />} />
+          <Route path="/games/word-matching/" element={<WordMatching/>}/>
         </Routes>
       </MainLayout>
     </Router>
