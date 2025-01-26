fdb5a7ca89f540b79e98f1031fc4939deb7d4b82
jimenezz22 <ljimenezbogantes@gmail.com>
2025-01-26 14:35:56 -0600

    Refactor address formatting utility to support customizable options

--- src/utils/helpers.jsx
+++ src/utils/helpers.jsx
@@ -1,3 +1,14 @@
-export const truncateAddress = (address) => {
-    return address ? `${address.slice(0, 6)}...${address.slice(-4)}` : '';
-  };
+export const formatAddress = (address, options = {}) => {
+  if (!address) return '';
+  
+  const { 
+    start = 6, 
+    end = 4, 
+    separator = '...',
+    fallback = 'Invalid address'
+  } = options;
+
+  return address.length > start + end 
+    ? `${address.slice(0, start)}${separator}${address.slice(-end)}`
+    : fallback;
+};
