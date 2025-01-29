ecc3e0cf98a70bbe0fdf85d7a9e93e388cbfe803
villarley <santivillarley1010@gmail.com>
2025-01-29 05:50:07 -0600

    feat: added questions object
    Includes:
    
    - Question : String
    - Options: Array[]
    - Answer : string

---
+++ src/components/quiz/quiz.jsx
@@ -0,0 +1,20 @@
+import React, { useState } from "react";
+
+
+const questions = [
+  {
+    question: "What is the past tense of 'go'?",
+    options: ["goed", "gone", "going", "went"],
+    answer: "went",
+  },
+  {
+    question: "Which word is a noun?",
+    options: ["run", "happy", "house", "quickly"],
+    answer: "house",
+  },
+  {
+    question: "What is the plural of 'child'?",
+    options: ["childs", "childes", "children", "childern"],
+    answer: "children",
+  },
+];
