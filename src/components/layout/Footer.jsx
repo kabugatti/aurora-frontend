d433a4dd034a0f535e497fa907be2e003788669c
ekumamatthew <ekumamatthew1@gmail.com>
2025-02-28 07:05:45 +0100

    added footer custom class

--- src/components/layout/Footer.jsx
+++ src/components/layout/Footer.jsx
@@ -2,7 +2,7 @@ import { Brain } from "lucide-react";
 
 const Footer = ({ customClass = "" }) => {
   return (
-    <footer className={`bg-blue-600 text-gray-400 py-5 `}>
+    <footer className={`bg-blue-600 text-gray-400 py-5 ${customClass}`}>
       <div className="max-w-6xl mx-auto px-4 sm:px-6 lg:px-8">
         <div className="flex items-center justify-between">
           <div className="flex items-center gap-2">
