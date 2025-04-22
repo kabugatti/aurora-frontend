818472c11894c89024810b6374adecd333cbaf77
matthew <ekumamatthew1@gmail.com>
2025-04-22 06:20:21 +0100

    rm/rf cta logo

--- src/pages/aurora-site/home/index.jsx
+++ src/pages/aurora-site/home/index.jsx
@@ -5,21 +5,6 @@ import React from "react";
 const HomePage = () => {
   return (
     <div className="min-h-screen bg-gray-50">
-      <nav className="bg-white border-b">
-        <div className="w-full px-4 sm:px-6 lg:px-8">
-          <div className="flex justify-between h-16 items-center">
-            <div className="flex items-center gap-2">
-              <img
-                src={AuroraIcon}
-                alt="Aurora Icon"
-                className="w-36 h-20 object-contain"
-              />
-              <span className="text-xl font-semibold text-gray-900"></span>
-            </div>
-          </div>
-        </div>
-      </nav>
-
       <CallToActionPage />
     </div>
   );
