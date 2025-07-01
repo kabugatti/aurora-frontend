962588452b0e4c980b50c8f6f0261657c5315fa9
nemalfaro29 <nalfaroperez29@gmail.com>
2025-06-30 19:39:43 -0600

    Added water brand

--- src/components/landing-page/hero-section/index.jsx
+++ src/components/landing-page/hero-section/index.jsx
@@ -1,11 +1,21 @@
 import CTAHeroButton from '../../ui/buttons/CTAHeroButton';
 import { heroContent } from '../call-to-action/Content';
 
-
 const HeroSection = () => {
   return (
-    <section className="w-full bg-[#030712] px-6 py-12" aria-label="Hero Section">
-      <div className="flex flex-col items-end text-right gap-4 max-w-7xl mx-auto px-4 sm:px-10 lg:px-12 py-8">
+    <section className="relative w-full bg-[#030712] px-6 py-12 overflow-hidden" aria-label="Hero Section">
+      
+      
+      <div className="absolute inset-0 z-0 pointer-events-none flex flex-col justify-evenly items-center text-white text-4xl font-light select-none opacity-5 whitespace-pre leading-[6rem]">
+        <p className="opacity-[0.14]">AURORA    AURORA    AURORA    AURORA    AURORA    AURORA</p>
+        <p className="opacity-[0.14]">AURORA    AURORA    AURORA    AURORA    AURORA    AURORA</p>
+        <p className="opacity-[0.14]">AURORA    AURORA    AURORA    AURORA    AURORA    AURORA</p>
+        <p className="opacity-[0.14]">AURORA    AURORA    AURORA    AURORA    AURORA    AURORA</p>
+      </div>
+
+
+      {/* Contenido principal */}
+      <div className="relative z-10 flex flex-col items-end text-right gap-4 max-w-7xl mx-auto px-4 sm:px-10 lg:px-12 py-8">
         <h2 className="flex flex-col gap-2 text-white font-bold text-3xl sm:text-4xl md:text-5xl lg:text-6xl">
           <span>{heroContent.title.textOne}</span>
           <span>{heroContent.title.textTwo}</span>
@@ -26,5 +36,4 @@ const HeroSection = () => {
   );
 };
 
-
-export default HeroSection; 
+export default HeroSection;
